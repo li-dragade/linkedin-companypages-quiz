9623b0dde40eadab0882fe9facc20487123f14b6
Dhananjay Ragade <dragade@linkedin.com>
2023-03-01 11:41:27 -0800

    initial version

---
+++ linkedin-companypages-quiz/contentscript.js
@@ -0,0 +1,14 @@
+// initialize the quiz when the extension loads
+
+// get the company id from the page
+console.log("hi from contentscript.js")
+
+// companyId = document.getElementById("organization-debug-entity-urn-id").innerHTML.split('\n')[1].trim()
+// console.log("companyId=" + companyId)
+
+// fetch(chrome.extension.getURL('data/data_' + companyId + '.json'))
+//   .then(response => response.json())
+//   .then(data => {
+//     document.getElementById("question").innerHTML = data[0].question
+//     document.getElementById("answer1").innerHTML = data[0].answers[0]
+//   });
